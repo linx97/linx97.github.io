6fa06358c0af5714ac53c8987707e148b2544bcc
Linx Latham <linxlatham@gmail.com>
2016-12-29 14:00:54 -0800

    🎉Initial Commit

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,39 @@
-$(window).scroll(function(){
-    
-    var wScroll = $(this).scrollTop();
-    
-    $('.title').css({
-        'transform' : 'translate(0px, '+ wScroll /3.5 +'%)'
-    });
-    
-    if(wScroll > $('.certs').offset().top - ($(window).height() / 1.3)) {
-    
-        $('.certs img').each(function(){
+$(document).ready(function(){
+
+    // $(window).scroll(function(){
         
-            $('.certs img').addClass('is-showing');
-        });
-    };
+    //     var wScroll = $(this).scrollTop();
+        
+    //     if(wScroll > $('.certs').offset().top - ($(window).height() / 1.3)) {
+        
+    //         $('.certs img').each(function(){
+            
+    //             $('.certs img').addClass('is-showing');
+    //         });
+    //     }
+    // });
+
+
+    var nav, yPos, collapse, brand;
+    function yScroll(){
+        nav = document.getElementById('nav');
+        collapse = document.getElementById('bs-example-navbar-collapse-1');
+        brand = document.getElementById('brand');
+        yPos = window.pageYOffset;
+        if(yPos > 100) {
+            nav.style.height = "85px";
+            nav.style.padding = "0 20px";
+            collapse.style.marginTop = "26px";
+            brand.style.height = "75px";
+        } else {
+            nav.style.height = "150px";
+            nav.style.padding = "0";
+            collapse.style.marginTop = "60px";
+            brand.style.height = "140px";
+        }
+    }
+        
+    window.addEventListener("scroll", yScroll);
+
 });
 
